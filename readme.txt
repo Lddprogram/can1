728477e976445fc663956d4c2f525c22433af0f6
Lddprogram <1442214284@qq.com>
2026-05-07 15:21:29 +0800

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,2 @@
+代码路径：
+~\单片机烧录代码\max&lis\examples\ble_peripheral\ble_app_uart\pca10040\s132\arm5_no_packs
---
+++ 单片机烧录代码/max&lis/components/softdevice/s132/hex/mpu6050.d
@@ -0,0 +1,4 @@
+..\..\..\..\..\..\components\softdevice\s132\hex\mpu6050.o: ..\..\..\MPU6050\mpu6050.c
+..\..\..\..\..\..\components\softdevice\s132\hex\mpu6050.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+..\..\..\..\..\..\components\softdevice\s132\hex\mpu6050.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+..\..\..\..\..\..\components\softdevice\s132\hex\mpu6050.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
